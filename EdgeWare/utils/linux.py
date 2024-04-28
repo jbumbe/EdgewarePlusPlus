ffa0308b7150f3538227c9339ab351cc9d273233
LewdDevelopment <158585445+LewdDevelopment@users.noreply.github.com>
2024-04-28 09:42:47 +0300

    Remove unnecessary Windows/Mac handling from linux.py

--- EdgeWare/utils/linux.py
+++ EdgeWare/utils/linux.py
@@ -240,62 +240,56 @@ def _get_desktop_environment():
     # and http://ubuntuforums.org/showthread.php?t=652320
     # and http://ubuntuforums.org/showthread.php?t=652320
     # and http://ubuntuforums.org/showthread.php?t=1139057
-    if sys.platform in ['win32', 'cygwin']:
-        return 'windows'
-    elif sys.platform == 'darwin':
-        return 'mac'
-    else:  # Most likely either a POSIX system or something not much common
-        desktop_session = os.environ.get('DESKTOP_SESSION')
-        if (
-            desktop_session is not None
-        ):  # easier to match if we doesn't have  to deal with caracter cases
-            desktop_session = desktop_session.lower()
-            if desktop_session in [
-                'gnome',
-                'unity',
-                'cinnamon',
-                'mate',
-                'xfce4',
-                'lxde',
-                'fluxbox',
-                'blackbox',
-                'openbox',
-                'icewm',
-                'jwm',
-                'afterstep',
-                'trinity',
-                'kde',
-            ]:
-                return desktop_session
-            ## Special cases ##
-            # Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE if using LXDE.
-            # There is no guarantee that they will not do the same with the other desktop environments.
-            elif 'xfce' in desktop_session or desktop_session.startswith('xubuntu'):
-                return 'xfce4'
-            elif desktop_session.startswith('ubuntustudio'):
-                return 'kde'
-            elif desktop_session.startswith('ubuntu'):
-                return 'gnome'
-            elif desktop_session.startswith('lubuntu'):
-                return 'lxde'
-            elif desktop_session.startswith('kubuntu'):
-                return 'kde'
-            elif desktop_session.startswith('razor'):  # e.g. razorkwin
-                return 'razor-qt'
-            elif desktop_session.startswith('wmaker'):  # e.g. wmaker-common
-                return 'windowmaker'
-            elif desktop_session.startswith('pop'):  # e.g. wmaker-common
-                return 'gnome'
-        if os.environ.get('KDE_FULL_SESSION') == 'true':
-            return 'kde'
-        elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-            if 'deprecated' not in os.environ.get('GNOME_DESKTOP_SESSION_ID'):  # type: ignore
-                return 'gnome2'
-        # From http://ubuntuforums.org/showthread.php?t=652320
-        elif _is_running('xfce-mcs-manage'):
+    desktop_session = os.environ.get('DESKTOP_SESSION')
+    if desktop_session is not None:
+        # easier to match if we doesn't have to deal with character cases
+        desktop_session = desktop_session.lower()
+        if desktop_session in [
+            'gnome',
+            'unity',
+            'cinnamon',
+            'mate',
+            'xfce4',
+            'lxde',
+            'fluxbox',
+            'blackbox',
+            'openbox',
+            'icewm',
+            'jwm',
+            'afterstep',
+            'trinity',
+            'kde',
+        ]:
+            return desktop_session
+        ## Special cases ##
+        # Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE if using LXDE.
+        # There is no guarantee that they will not do the same with the other desktop environments.
+        elif 'xfce' in desktop_session or desktop_session.startswith('xubuntu'):
             return 'xfce4'
-        elif _is_running('ksmserver'):
+        elif desktop_session.startswith('ubuntustudio'):
             return 'kde'
+        elif desktop_session.startswith('ubuntu'):
+            return 'gnome'
+        elif desktop_session.startswith('lubuntu'):
+            return 'lxde'
+        elif desktop_session.startswith('kubuntu'):
+            return 'kde'
+        elif desktop_session.startswith('razor'):  # e.g. razorkwin
+            return 'razor-qt'
+        elif desktop_session.startswith('wmaker'):  # e.g. wmaker-common
+            return 'windowmaker'
+        elif desktop_session.startswith('pop'):  # e.g. wmaker-common
+            return 'gnome'
+    if os.environ.get('KDE_FULL_SESSION') == 'true':
+        return 'kde'
+    elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
+        if 'deprecated' not in os.environ.get('GNOME_DESKTOP_SESSION_ID'):  # type: ignore
+            return 'gnome2'
+    # From http://ubuntuforums.org/showthread.php?t=652320
+    elif _is_running('xfce-mcs-manage'):
+        return 'xfce4'
+    elif _is_running('ksmserver'):
+        return 'kde'
     return 'unknown'
 
 def does_desktop_shortcut_exist(name: str):
